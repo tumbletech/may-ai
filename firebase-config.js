0fd06f56d1234e0509b30919d506b15e84c270eb
Tumbletech, Inc. <112755396+tumbletech@users.noreply.github.com>
2025-11-11 15:36:41 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
@@ -16,4 +16,3 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-</script>
