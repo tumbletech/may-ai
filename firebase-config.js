3d2bed8908b264e5d75fdddf98d178b27798e695
Tumbletech, Inc. <112755396+tumbletech@users.noreply.github.com>
2025-11-11 15:23:11 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNTjyJ1pPks9SrH5LUmwiH3aS-74RAo3I",
+    authDomain: "may-ai-4e746.firebaseapp.com",
+    projectId: "may-ai-4e746",
+    storageBucket: "may-ai-4e746.firebasestorage.app",
+    messagingSenderId: "443014844695",
+    appId: "1:443014844695:web:69004bb86cea420c992752"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
